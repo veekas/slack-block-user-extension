b74a40239160a2d73a172ecfcf5f1595c712b6e3
Veekas Ashoka <veekas@veekasmeansprogress.com>
2020-02-17 12:19:19 -0500

    remove blocked avatars from thread previews

--- README.md
+++ README.md
@@ -5,3 +5,9 @@ A browser extension for Google Chrome
 ## Install
 
 https://chrome.google.com/webstore/detail/slack-block-user/jllpceiekdglclebfdnhlbolhegodpld
+
+## Context
+
+This is a fork of [phonglk's Slack Block User extension](https://github.com/phonglk/slack-block-user-extension). Currently that extensions doesn't work. If that repo publishes the changes in this PR to the Chrome Web Store, this extension will be removed.
+
+Contribute to the fork here: https://github.com/veekas/slack-block-user-extension
--- src/content/messageList.ts
+++ src/content/messageList.ts
@@ -4,6 +4,8 @@ import { observeDOM, tryTillSuccess } from "../common/utils";
 
 const MAIN_MESSAGE_LIST_SELECTOR = '.c-message_list .c-virtual_list__item'
 const THREAD_MESSAGE_LIST_SELECTOR = '.p-workspace__secondary_view .c-virtual_list__item'
+const THREAD_PREVIEW_AVATAR_SELECTOR = '.c-avatar__image'
+const THREAD_PREVIEW_AVATAR_BUTTON_SELECTOR = '.c-avatar--interactive';
 
 const getUserIdFromVirtualItem = (node: Element) => {
   let userId: string
@@ -22,10 +24,27 @@ const getUserIdFromVirtualItem = (node: Element) => {
   return userId
 }
 
+const removeThreadPreviewAvatars = (workspace: Workspace) => {
+  const avatarNodes = document.querySelectorAll(THREAD_PREVIEW_AVATAR_BUTTON_SELECTOR);
+  const blackList = workspace.blackList.list;
+  let isBlocked = false;
+  let userId: string
+
+  Array.from(avatarNodes).forEach((node) => {
+    const $avatar = node.querySelector('img') as HTMLImageElement;
+    const idMatch = $avatar.src.match(/slack[^.]*\.com\/[^-]+-([^-]+)/);
+    if (!idMatch) return false;
+    userId = idMatch[1];
+    isBlocked = blackList.has(userId);
+    (node as HTMLImageElement).style.display = isBlocked ? 'none' : 'block'
+  });
+}
+
 const createMessageListFilterer = (workspace: Workspace) => async () => {
   const messageNodes = document.querySelectorAll(MAIN_MESSAGE_LIST_SELECTOR)
   const blackList = workspace.blackList.list
   let isBlocked = false;
+
   Array.from(messageNodes).forEach(node => {
       const $div = node.querySelector('div')
       if (!$div || !($div.classList.contains('c-message') || $div.classList.contains('c-message_kit__message'))) return;
@@ -88,7 +107,10 @@ export default async function startMessageListHook(workspace: Workspace) {
     const $messageList = document.body.querySelector('.c-message_list .c-virtual_list__scroll_container')
     if (!$messageList) return setTimeout(startObserve, 200)
     const messageListFilterer = throttle(
-      createMessageListFilterer(workspace),
+      () => {
+        createMessageListFilterer(workspace);
+        removeThreadPreviewAvatars(workspace);
+      },
       500
     )
     observeDOM($messageList, { childList: true, subtree: false }, messageListFilterer)
